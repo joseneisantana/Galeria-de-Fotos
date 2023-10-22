00b2257b2807fe3c9fecfbac1ada2479f2974419
Josenei Santana <joseneisantana@yahoo.com>
2023-10-22 12:38:54 -0300

    Adcionando animação jQuery

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+     $('header button').click(function(){
+        $('form').slideDown();
+     })
+
+     $('#botao-cancelar').click(function(){
+       $('form').slideUp();
+     })
+
+
+     $('form').on('submit',function(e){
+       console.log("submint");
+       e.preventDefault();
+     })
+})
+
